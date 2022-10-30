973bfd81a11fd9353109fd7975033fd4105dd334
wighawag <wighawag@gmail.com>
2022-10-30 17:33:47 +0000

    use global setup

--- test/Blockies.test.ts
+++ test/Blockies.test.ts
@@ -1,19 +1,5 @@
 import {expect} from './chai-setup';
-import {ethers, deployments, getUnnamedAccounts} from 'hardhat';
-import {setupUsers} from './utils/users';
-import {Blockies} from '../typechain';
-
-const setup = deployments.createFixture(async () => {
-	await deployments.fixture('Blockies');
-	const contracts = {
-		Blockies: <Blockies>await ethers.getContract('Blockies')
-	};
-	const users = await setupUsers(await getUnnamedAccounts(), contracts);
-	return {
-		...contracts,
-		users
-	};
-});
+import {setup} from './setup';
 
 describe('Blockies', function () {
 	it('works', async function () {
