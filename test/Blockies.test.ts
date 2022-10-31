e21652f73ccd8eb59b89c501129662291c5ff1fb
wighawag <wighawag@gmail.com>
2022-10-31 12:02:00 +0000

    more test

--- test/Blockies.test.ts
+++ test/Blockies.test.ts
@@ -3,6 +3,12 @@ import {setup} from './setup';
 import {waitFor} from './utils';
 
 describe('Blockies', function () {
+	it('has correct balance from the start', async function () {
+		const state = await setup();
+		const {users, Blockies} = state;
+		expect(await Blockies.balanceOf(users[0].address)).to.be.equal(1);
+	});
+
 	it('has correct balance after emitting first transfer event', async function () {
 		const state = await setup();
 		const {users, Blockies} = state;
@@ -25,5 +31,32 @@ describe('Blockies', function () {
 		await waitFor(users[0].Blockies.transferFrom(users[0].address, users[1].address, users[1].address));
 		expect(await Blockies.balanceOf(users[0].address), 'users[0]').to.be.equal(1);
 		expect(await Blockies.balanceOf(users[1].address), 'users[1]').to.be.equal(1);
+	});
+
+	it('has correct balance after receiving 3 nft', async function () {
+		const state = await setup();
+		const {users, Blockies} = state;
+		await waitFor(users[1].Blockies.transferFrom(users[1].address, users[0].address, users[1].address));
+		await waitFor(users[2].Blockies.transferFrom(users[2].address, users[0].address, users[2].address));
+		await waitFor(users[3].Blockies.transferFrom(users[3].address, users[0].address, users[3].address));
+		expect(await Blockies.balanceOf(users[0].address), 'users[0]').to.be.equal(4);
+	});
+
+	it('has correct balance after receiving 3 nft and sending 4', async function () {
+		const state = await setup();
+		const {users, Blockies} = state;
+		await waitFor(users[1].Blockies.transferFrom(users[1].address, users[0].address, users[1].address));
+		await waitFor(users[2].Blockies.transferFrom(users[2].address, users[0].address, users[2].address));
+		await waitFor(users[3].Blockies.transferFrom(users[3].address, users[0].address, users[3].address));
+		await waitFor(users[0].Blockies.transferFrom(users[0].address, users[1].address, users[1].address));
+		await waitFor(users[0].Blockies.transferFrom(users[0].address, users[2].address, users[2].address));
+		await waitFor(users[0].Blockies.transferFrom(users[0].address, users[3].address, users[3].address));
+		await waitFor(users[0].Blockies.transferFrom(users[0].address, users[4].address, users[0].address));
+
+		expect(await Blockies.balanceOf(users[0].address), 'users[0]').to.be.equal(0);
+		expect(await Blockies.balanceOf(users[1].address), 'users[1]').to.be.equal(1);
+		expect(await Blockies.balanceOf(users[2].address), 'users[2]').to.be.equal(1);
+		expect(await Blockies.balanceOf(users[3].address), 'users[3]').to.be.equal(1);
+		expect(await Blockies.balanceOf(users[4].address), 'users[4]').to.be.equal(2);
 	});
 });
