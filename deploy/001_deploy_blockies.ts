13a91d334adb16068755cc185bdd6e6c0f74417b
wighawag <wighawag@gmail.com>
2022-11-02 20:31:40 +0000

    new deployment on goerli

--- deploy/001_deploy_blockies.ts
+++ deploy/001_deploy_blockies.ts
@@ -13,8 +13,8 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 		log: true,
 		args: [initialOwnerOfBlockyZero],
 		// proxy: network.name !== 'mainnet',
-		// "0x0200","0x0197","0x05","0x09","0x10","0x13","0x19","0x24","0x97","0x88","0x0167","0x0147","0x0179","0x0161","0x0139","0x65","0x0137"
-		deterministicDeployment: '0x0137',
+		// "0x0200",,"0x05","0x88","0x0167","0x0147","0x0161","0x65","0x0137"
+		deterministicDeployment: '0x65',
 		autoMine: true,
 		skipIfAlreadyDeployed: hre.network.live,
 	});
